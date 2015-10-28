71c326bff00d44703a5b5f75eeb0b71f63ea162f
Jit <kanetkarster@gmail.com>
2015-10-28 12:34:28 -0400

    updated filter

--- src/moving_average.c
+++ src/moving_average.c
@@ -1,32 +1,22 @@
 #include "moving_average.h"
 
-#define BUFF_SIZE 20
-
-typedef struct {
-	float buffer[BUFF_SIZE];
-	int replace;
-	float avg;
-	float sum;
-} FilterBuffer;
-
-FilterBuffer filter;
 
 /*!
 	adds a value to the filter
 	@param f value to be added to buffer
  */
-int add_value(float f) {
-	filter.sum -= filter.buffer[filter.replace % BUFF_SIZE];
-	filter.sum += (filter.buffer[filter.replace % BUFF_SIZE] = f);
-	filter.avg = filter.replace++ < BUFF_SIZE 
-											? filter.sum/filter.replace 
-											: filter.sum/BUFF_SIZE;
+int add_value(FilterBuffer *buf, float f) {
+	buf->sum -= buf->buffer[buf->replace % BUFF_SIZE];
+	buf->sum += (buf->buffer[buf->replace % BUFF_SIZE] = f);
+	buf->avg = buf->replace++ < BUFF_SIZE 
+											? buf->sum/buf->replace 
+											: buf->sum/BUFF_SIZE;
 	return 0;
 }
 
 /*!
 	@return average value
  */
-float get_value(void) {
-	return filter.avg;
+float get_value(FilterBuffer *buf) {
+	return buf->avg;
 }
