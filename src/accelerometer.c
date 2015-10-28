2908c753d722d1e5aa158d997972241c158d31fa
Jit <kanetkarster@gmail.com>
2015-10-28 12:45:10 -0400

    added get_angle() function to accelerometer

--- src/accelerometer.c
+++ src/accelerometer.c
@@ -81,7 +81,7 @@ void EXTI0_IRQHandler(void)
 	if (EXTI_GetITStatus(LIS3DSH_SPI_INT1_EXTI_LINE) != RESET){
 		LIS3DSH_ReadACC(xyz);
 		//printf("x: %f\ty: %f\tz: %f\n", xyz[0], xyz[1], xyz[2]);
-		float x = xyz[0], y = xyz[1] - 13.5, z = xyz[2] - 26;
+		float x = xyz[0], y = xyz[1] - 13.5f, z = xyz[2] - 26.0f;
 		float angle = atan2f(x, sqrtf(y*y + z*z));
 		//printf("%f\n", angle);
 		//float angle = (xyz[2] - 26) / (Z_POS_ZVG - Z_NEG_ZVG) * 180 + 90;
@@ -98,5 +98,6 @@ void EXTI0_IRQHandler(void)
 	@return 0 on success, else negative
  */
 int get_angle(int* angle) {
+	*angle = (int) get_value(&angle_avg);
 	return 0;
 }
